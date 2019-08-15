ce3259ba2aceb72f7826553776e0ebc7ec15e34a
TrybeCoder <39412590+TrybeCoder@users.noreply.github.com>
2019-08-15 16:50:53 +0800

    Create 0010.cpp

---
+++ 0010.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty())
+            return s.empty();
+        
+        bool first_match = bool(s.size()) && (p[0] == s[0] || p[0] == '.');
+        
+        if(p.size()>=2 && p[1] == '*')
+            return isMatch(s, p.substr(2)) || 
+            (first_match && isMatch(s.substr(1),p));
+        return first_match && isMatch(s.substr(1),p.substr(1));
+    }
+};
